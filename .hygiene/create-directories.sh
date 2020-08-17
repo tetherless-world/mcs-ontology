fa714aa7ec92e6fc8c3c28b5355d61b6c2ab2834
Henrique O. Santos <hensantos@yahoo.com>
2020-08-17 19:24:35 -0400

    debug

--- .hygiene/create-directories.sh
+++ .hygiene/create-directories.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir /output
+pwd
 docker exec -d mcs_hygiene mkdir /input
 docker exec -d mcs_hygiene mkdir /output
-docker exec -d mcs_hygiene mkdir /output/mcs_ontology
 docker exec -d mcs_hygiene mkdir /var/tmp
