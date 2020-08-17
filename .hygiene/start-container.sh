89cdfa7d81312e5e73f386b0b124778f8b3ec915
Henrique O. Santos <hensantos@yahoo.com>
2020-08-17 18:41:30 -0400

    fix dir

--- .hygiene/start-container.sh
+++ .hygiene/start-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mcs_dir=${mcs_dir:-"$( cd "$( dirname "${BASH_SOURCE[0]}/.." )" >/dev/null 2>&1 && pwd )"}
+mcs_dir=${mcs_dir:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"}
 
 export ONTPUB_FAMILY="${mcs_dir##*/}"
 export WORKSPACE="$(cd "${mcs_dir}" && cd .. && pwd)"
