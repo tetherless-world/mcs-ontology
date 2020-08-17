497eed726954688ce578d33d32c0dfb6a6e099b1
Henrique O. Santos <hensantos@yahoo.com>
2020-08-17 15:46:04 -0400

    run detached

--- run-hygiene.sh
+++ run-hygiene.sh
@@ -5,8 +5,10 @@ mcs_dir=${mcs_dir:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && 
 export ONTPUB_FAMILY="${mcs_dir##*/}"
 export WORKSPACE="$(cd "${mcs_dir}" && cd .. && pwd)"
 
+mkdir /output
+
 cd "$mcs_dir/docker/compose/hygiene"
 docker-compose pull mcs_hygiene
 
 # Run the hygiene script
-docker-compose run mcs_hygiene
+docker-compose run -d mcs_hygiene
