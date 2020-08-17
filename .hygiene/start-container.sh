2204797f8166cc9c7d266239e639fe39c3ded165
Henrique O. Santos <hensantos@yahoo.com>
2020-08-17 18:26:05 -0400

    move and rename start script

--- .hygiene/start-container.sh
+++ .hygiene/start-container.sh
@@ -5,8 +5,6 @@ mcs_dir=${mcs_dir:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && 
 export ONTPUB_FAMILY="${mcs_dir##*/}"
 export WORKSPACE="$(cd "${mcs_dir}" && cd .. && pwd)"
 
-mkdir /output
-
 cd "$mcs_dir/docker/compose/hygiene"
 docker-compose pull mcs_hygiene
 
